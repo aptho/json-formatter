92276dcd1548016d898daa330d19d4450acd0aed
Andrew Thompson <andrew-thompson@hotmail.co.uk>
2020-07-16 10:34:08 +0100

    Change endpoint, filenames

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = 3000
 const path = require('path')
 
 app.use('/dist', express.static('dist'))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/pages/index.html`))
+app.get('/json', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/pages/json.html`))
 })
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`))
